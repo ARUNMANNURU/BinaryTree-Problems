8701df0e7b9d83d468c7fd25aa168612cee1c408
Mannuru <arunm057@gmail.com>
2017-07-21 13:15:28 -0400

    preorder

---
+++ src/IterativePreOrder.java
@@ -0,0 +1,70 @@
+import java.util.Stack;
+
+/**
+ * 
+ */
+
+/**
+ * @author ArunMannuru
+ *
+ */
+public class IterativePreOrder {
+
+	/**
+	 * @param args
+	 */
+	
+	  Node root;
+	     
+	    void iterativePreorder()
+	    {
+	        iterativePreorder(root);
+	    }
+	 
+	    // An iterative process to print preorder traversal of Binary tree
+	    void iterativePreorder(Node node) {
+	         
+	        // Base Case
+	        if (node == null) {
+	            return;
+	        }
+	 
+	        // Create an empty stack and push root to it
+	        Stack<Node> nodeStack = new Stack<Node>();
+	        nodeStack.push(root);
+	 
+	        /* Pop all items one by one. Do following for every popped item
+	         a) print it
+	         b) push its right child
+	         c) push its left child
+	         Note that right child is pushed first so that left is processed first */
+	        while (nodeStack.empty() == false) {
+	             
+	            // Pop the top item from stack and print it
+	            Node mynode = nodeStack.peek();
+	            System.out.print(mynode.key + " ");
+	            nodeStack.pop();
+	 
+	            // Push right and left children of the popped node to stack
+	            if (mynode.right != null) {
+	                nodeStack.push(mynode.right);
+	            }
+	            if (mynode.left != null) {
+	                nodeStack.push(mynode.left);
+	            }
+	        }
+	    }
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IterativePreOrder tree = new IterativePreOrder();
+        tree.root = new Node(10);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(2);
+        tree.root.left.left = new Node(3);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(2);
+        tree.iterativePreorder();
+	}
+
+}
